c69720c10bc12648ff12dc135e1d536316354bc6
salah856 <salah_elhossiny856@yahoo.com>
2021-11-05 03:40:25 +0200

    edit port of server

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use('/api', router);
 
 
-app.listen(3000, function () {
+app.listen(4000, function () {
     console.log('Example app listening on port 3000!');
     MongoConnection.connect(app);
 });
