8c362f7d0ed977a89e7cc48199ef90f23eac0f49
salah856 <salah_elhossiny856@yahoo.com>
2021-11-05 11:22:11 +0200

    added feat! components

--- Frontend/exam-app/src/App.js
+++ Frontend/exam-app/src/App.js
@@ -2,18 +2,29 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+  useHistory,
+} from "react-router-dom";
+
+
 import rootReducer from './redux/reducers/index';
+import QuestionsStepper from './components/questionsStepper';
 
 
 let store = createStore(rootReducer);
 
 function App() {
 
+  const history = useHistory();
+
   const [userName, setUserName] = useState('');
   useEffect(() => {
     let name = prompt('Enter your name');
     
-    name && setUserName(name);
+    name && setUserName(name) && history.push('/questions');
    
   }, []);
 
@@ -22,6 +33,19 @@ function App() {
       <div className="App">
         <header className="App-header">
         </header>
+        <Router>
+          <Switch>
+            <Route path="/">
+
+            </Route>
+            <Route path="/questions">
+              <QuestionsStepper />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </Provider>
   );
