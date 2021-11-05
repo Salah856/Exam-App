2c39cc6fd02922f81d6cd3837b84558933a88a2c
salah856 <salah_elhossiny856@yahoo.com>
2021-11-05 09:48:56 +0200

    adding mock db

---
+++ Frontend/exam-app/src/db.js
@@ -0,0 +1,59 @@
+export const questions = [
+
+    {
+        text: "What is the name of Egypt capital? ", 
+        answers: ["Cairo", "Alex", "USA"],
+        correct: 'Cairo',
+    }, 
+    {
+        text: 'What is the name of USA capital?', 
+        answers: ["New York", "Washington", "London"],
+        correct: 'Washington',
+    }, 
+    {
+        text: 'What is the name of England capital?', 
+        answers: ["LiverPool", "Manchester", "London"],
+        correct: 'London',
+    }, 
+    {
+        text: 'The water pollution in river is measured by the dissolved amount of?', 
+        answers: ["Oxygen", "Nitrogen", "Ozone"],
+        correct: 'Oxygen',
+    }, 
+    {
+        text: 'Ultraviolet rays can be used in water treatment as?', 
+        answers: ["Hydrolyser", "Disinfectant", "Precipitator"],
+        correct: 'Disinfectant',
+    }, 
+    {
+        text: "'OS' computer abbreviation usually means ?", 
+        answers: ["Operating System", "Open Software", "Optical Sensor"],
+        correct: 'Operating System',
+    }, 
+    {
+        text: 'In which year was the SPICE simulator introduced?', 
+        answers: ["1996", "1972", "1800"],
+        correct: '1972',
+    }, 
+    {
+        text: 'The purpose of choke in tube light is ?', 
+        answers: [
+            "To decrease the voltage momentarily",
+            "To increase the voltage momentarily",
+            "To decrease the current"
+        ],
+        correct: 'To increase the voltage momentarily',
+    }, 
+    {
+        text: 'Who developed Yahoo?', 
+        answers: ["Mina Adam", "Salah Elhossiny", "David Filo"],
+        correct: 'David Filo',
+    }, 
+    {
+        text: 'Which of the following is used in pencils?', 
+        answers: ["Graphite", "Silicon", "Charcoal"],
+        correct: 'Graphite',
+    }, 
+
+]
+
