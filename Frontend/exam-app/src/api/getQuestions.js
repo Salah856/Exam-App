03bf0eb6381ccc59ab867a61dd404c14ab2a6eda
salah856 <salah_elhossiny856@yahoo.com>
2021-11-05 09:31:37 +0200

    adding api axioss to get questions from backend

--- Frontend/exam-app/src/api/getQuestions.js
+++ Frontend/exam-app/src/api/getQuestions.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-let BASE_URL = 'http://localhost:8080/api/v1/';
+let BASE_URL = 'http://localhost:4000/api/';
 
 export const getQuestions = () => {
-    return axios.get(`/api/exam/${examId}/questions`)
+    return axios.get(`${BASE_URL}/questions`)
         .then(res => res.data)
 }
+
+
 
